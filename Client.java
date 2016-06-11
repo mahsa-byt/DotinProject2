d0f3979b0b90b1a380df06bcffb6de33add08e03
Sajedeh Panahi <sajedehpanahi@gmail.com>
2016-06-11 17:05:55 +0430

    client

--- Client.java
+++ Client.java
@@ -3,23 +3,21 @@ import java.io.*;
 import java.net.Socket;
 
 public class Client {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Socket socket;
         try {
-
-            //InetAddress inetAddress = InetAddress.getByName("192.168.4.75");
-            socket = new Socket("localhost", 8080);
-
             XMLParser xmlParser = new XMLParser();
             Terminal terminal = xmlParser.readXMLFile();
 
+            socket = new Socket(terminal.getServerIP(), terminal.getServerPort());
+
             ObjectOutputStream clientOutputStream = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream  clientInputStream  = new ObjectInputStream(socket.getInputStream());
+            ObjectInputStream clientInputStream = new ObjectInputStream(socket.getInputStream());
             clientOutputStream.writeObject(terminal);
 
-            terminal= (Terminal) clientInputStream.readObject();
+            terminal = (Terminal) clientInputStream.readObject();
 
-            for (int i=0;i<terminal.getTransactions().size();i++){
+            for (int i = 0; i < terminal.getTransactions().size(); i++) {
                 System.out.println(terminal.getTransactions().get(i).getAmount());
             }
 
@@ -27,11 +25,9 @@ public class Client {
             clientOutputStream.close();
 
             socket.close();
-        }
-        catch (EOFException e) {
+        } catch (EOFException e) {
             e.printStackTrace();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         } catch (XMLStreamException e) {
             e.printStackTrace();
